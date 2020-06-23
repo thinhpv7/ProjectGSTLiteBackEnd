072db01b855322e2c1eb277511d6229863604255
Pham Phu Thinh <thinhpv7@gmail.com>
2020-06-23 21:59:15 +0700

    First commit

---
+++ src/main/java/com/abc/xyz/ProjectGstLiteAbcxyzApplication.java
@@ -0,0 +1,13 @@
+package com.abc.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGstLiteAbcxyzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectGstLiteAbcxyzApplication.class, args);
+    }
+
+}
